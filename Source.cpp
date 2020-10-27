c16ffb8da6466de786397e511ede1558d497da5f
Ali Tamer Ünal <atu@icrontech.com>
2020-10-27 12:26:00 +0300

    V1.

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
+int FindSimpleWithIndexedForLoop(vector<int>& v, int ValueToFind)
+{
+	for (int j = 0; j < v.size(); j++)
+	{
+		if (v[j] == ValueToFind)
+			return j;
+	}
+	return -1;
+}
 
 void main()
 {
-	cout << "My name is Ali Tamer.";
+	vector<int> v = { 1, 3, 5, 8, 11, 15 };
+	cout << " FindSimpleWithIndexedForLoop found: " << FindSimpleWithIndexedForLoop(v, 17);
 }
