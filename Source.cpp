d5d939f44f5fed01f54a79766fde9d51c1025cb1
Ali Tamer Ünal <atu@icrontech.com>
2020-11-03 13:57:50 +0300

    End of lecture v1.

--- Source.cpp
+++ Source.cpp
@@ -3,36 +3,19 @@
 
 using namespace std;
 
-int FindSimpleWithIndexedForLoop(vector<int>& v, int ValueToFind)
+int FindSimpleWithIndexedForLoop(vector<int>& c, int ValueToFind)
 {
-	for (int j = 0; j < v.size(); j++)
+	for (int j = 0; j < c.size(); j++)
 	{
-		if (v[j] == ValueToFind)
+		if (c[j] == ValueToFind)
 			return j;
 	}
 	return -1;
 }
 
-// [return type or void] [function name] ( optional list of parameters)
-void f()
-{
-	cout << "f";
-}
-
-int g(int x)
-{
-	return x + 1;
-}
-
-
 
 void main()
 {
-	f();
-	cout << g(5);
-
-	cout << sizeof(int) << " " << sizeof(double) << " " << sizeof(long long);
-
-	//vector<int> v = { 1, 3, 5, 8, 11, 15 };
-	//cout << " FindSimpleWithIndexedForLoop found: " << FindSimpleWithIndexedForLoop(v, 17);
+	vector<int> v = { 1, 3, 5, 8, 11, 15 };
+	cout << " FindSimpleWithIndexedForLoop found: " << FindSimpleWithIndexedForLoop(v, 3);
 }
