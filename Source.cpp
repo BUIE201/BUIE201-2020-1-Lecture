360ac61659b1bf878296e892051741228df3642c
Ali Tamer Ünal <atu@icrontech.com>
2020-11-25 23:16:46 +0300

    Separated member function declarations from definitions.

--- Source.cpp
+++ Source.cpp
@@ -14,21 +14,9 @@ class Graph
 	map<pair<Node*, Node*>, Arc*> Arcs;
 
 public:
-	void AddNode(Node* p)
-	{
-		Nodes.push_back(p);
-	}
-	void AddArc(Node* FromNode, Node* ToNode, Arc* p)
-	{
-		Arcs.emplace(make_pair(FromNode, ToNode), p);
-	}
-	Arc* GetArc(Node* FromNode, Node* ToNode)
-	{
-		auto it = Arcs.find({ FromNode, ToNode });
-		if (it == Arcs.end())
-			return nullptr;
-		return it->second;
-	}
+	void AddNode(Node* p);
+	void AddArc(Node* FromNode, Node* ToNode, Arc* p);
+	Arc* GetArc(Node* FromNode, Node* ToNode);
 };
 
 class Node
@@ -40,64 +28,11 @@ class Node
 
 public:
 	// Constructor
-	Node(int IDIn, Graph& g) : ID(IDIn), pG(&g)
-	{
-		g.AddNode(this);
-	}
-	void AddSuccessor(Node* p)
-	{
-		Successors.push_back(p);
-	}
-	int GetShortestPathFromCache(Node* ToNode)
-	{
-		auto it = Cache.find(ToNode);
-		if (it == Cache.end())
-			return -1;
-		return it->second;
-	}
-	int RegisterShortestPathFromCache(Node* ToNode, int dist)
-	{
-		Cache.emplace(ToNode, dist);
-	}
-
-	int ShortestPathLength(Node* ToNode)
-	{
-		// Simplified version of Bellman-Ford algorithm : Valid for DAGs with positive distances.
-		// Dynamic programming
-
-		if (this == ToNode)
-			return 0;
-
-		if (Successors.empty())
-			return INT_MAX;
-
-		int MinLength = INT_MAX;
-		for (Node* SuccessorNode : Successors)
-		{
-			// memoization, cache
-			int dist = 0;
-			int SPL = SuccessorNode->GetShortestPathFromCache(ToNode);
-			if (SPL > 0)
-			{
-				dist = SPL;
-			}
-			else
-			{
-				dist = ShortestPathLength(ToNode);
-				SuccessorNode->RegisterShortestPathFromCache(ToNode, dist);
-			}
-
-			if (dist != INT_MAX)
-			{
-				Arc* pA = pG->GetArc(this, SuccessorNode);
-				auto PL = pA->GetDistance() + dist;
-				if (PL < MinLength)
-					MinLength = PL;
-			}
-		}
-		return MinLength;
-	}
-
+	Node(int IDIn, Graph& g);
+	void AddSuccessor(Node* p);
+	int GetShortestPathFromCache(Node* ToNode);
+	void RegisterShortestPathFromCache(Node* ToNode, int dist);
+	int ShortestPathLength(Node* ToNode);
 };
 
 class Arc
@@ -109,13 +44,92 @@ class Arc
 	int Distance;
 
 public:
-	Arc(Node* F, Node* T, int d, Graph& g) : FromNode(F), ToNode(T), pG(&g), Distance(d)
-	{
-		g.AddArc(F, T, this);
-		F->AddSuccessor(T);
-	}
+	Arc(Node* F, Node* T, int d, Graph& g);
 	int GetDistance() { return Distance; }
 };
+
+
+void Graph::AddNode(Node* p)
+{
+	Nodes.push_back(p);
+}
+void Graph::AddArc(Node* FromNode, Node* ToNode, Arc* p)
+{
+	Arcs.emplace(make_pair(FromNode, ToNode), p);
+}
+Arc* Graph::GetArc(Node* FromNode, Node* ToNode)
+{
+	auto it = Arcs.find({ FromNode, ToNode });
+	if (it == Arcs.end())
+		return nullptr;
+	return it->second;
+}
+
+Node::Node(int IDIn, Graph& g) : ID(IDIn), pG(&g)
+{
+	g.AddNode(this);
+}
+void Node::AddSuccessor(Node* p)
+{
+	Successors.push_back(p);
+}
+int Node::GetShortestPathFromCache(Node* ToNode)
+{
+	auto it = Cache.find(ToNode);
+	if (it == Cache.end())
+		return -1;
+	return it->second;
+}
+void Node::RegisterShortestPathFromCache(Node* ToNode, int dist)
+{
+	Cache.emplace(ToNode, dist);
+}
+
+int Node::ShortestPathLength(Node* ToNode)
+{
+	// Simplified version of Bellman-Ford algorithm : Valid for DAGs with positive distances.
+	// Dynamic programming
+
+	if (this == ToNode)
+		return 0;
+
+	if (Successors.empty())
+		return INT_MAX;
+
+	int MinLength = INT_MAX;
+	for (Node* SuccessorNode : Successors)
+	{
+		// memoization, cache
+		int dist = 0;
+		int SPL = SuccessorNode->GetShortestPathFromCache(ToNode);
+		if (SPL > 0)
+		{
+			dist = SPL;
+		}
+		else
+		{
+			dist = SuccessorNode->ShortestPathLength(ToNode);
+			SuccessorNode->RegisterShortestPathFromCache(ToNode, dist);
+		}
+
+		if (dist != INT_MAX)
+		{
+			Arc* pA = pG->GetArc(this, SuccessorNode);
+			auto PL = pA->GetDistance() + dist;
+			if (PL < MinLength)
+				MinLength = PL;
+		}
+	}
+	return MinLength;
+}
+
+Arc::Arc(Node* F, Node* T, int d, Graph& g) : FromNode(F), ToNode(T), pG(&g), Distance(d)
+{
+	g.AddArc(F, T, this);
+	F->AddSuccessor(T);
+}
+
+
 
 int main()
 {
@@ -136,7 +150,7 @@ int main()
 	Arc a5(&n3, &n5, 2, g);
 	Arc a6(&n4, &n5, 1, g);
 
-	int pathlength = n1.ShortestPathLength(&n5);
+	int pathlength = n0.ShortestPathLength(&n5);
 
 	return 1;
 }
