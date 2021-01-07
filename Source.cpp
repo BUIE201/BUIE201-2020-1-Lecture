c525029a08c4d7e4a435565937d380234e153199
Ali Tamer Ünal <atu@icrontech.com>
2021-01-07 08:43:10 +0300

    First commit.

--- Source.cpp
+++ Source.cpp
@@ -1,184 +1,112 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-
-class MyData
+struct Node
 {
-	string Name;
 	int i;
+	Node* pLeft;
+	Node* pRight;
 
-public:
-	MyData() : i(-1) {}
-	MyData(int x, string NameIn) : i(x), Name(NameIn) {}
-	MyData(int x) : i(x) {}
-
-	static MyData null() { return MyData(); }
-	bool IsNull() { return i == -1; }
-
-	bool operator==(MyData& d)
-	{
-		return i == d.i;
-	}
-	friend ostream& operator<<(ostream& os, MyData& d);
+	Node(int iIn) : i(iIn), pLeft(nullptr), pRight(nullptr) {}
 };
 
-ostream& operator<<(ostream& os, MyData& d)
+void InsertToTree(Node*& pRoot, Node* pNew)
 {
-	if (d.IsNull())
-		os << "null";
+	if (!pRoot)
+	{
+		pRoot = pNew;
+		return;
+	}
+
+	if (pNew->i <= pRoot->i)
+		InsertToTree(pRoot->pLeft, pNew);
 	else
-		os << d.Name << ":" << d.i;
-	return os;
+		InsertToTree(pRoot->pRight, pNew);
 }
 
-template <typename T> class ListNode
+void DeleteNodeWithTwoChildren(Node*& q, Node*& p)
 {
-public:
-	T data;
-	shared_ptr<ListNode> Next;
-
-	ListNode(T d) : data(d), Next(nullptr) {}
-	bool operator==(ListNode& o)
+	if (p->pRight)
 	{
-		return data == o.data;
+		DeleteNodeWithTwoChildren(q, p->pRight);
+		return;
 	}
-};
 
-template <typename T> class List
+	p->i = q->i;
+	q = p;
+	p = p->pLeft;
+}
+
+void DeleteNodeFromTree(Node*& pRoot, int i)
 {
-	shared_ptr< ListNode<T> > Head{ nullptr };
-	shared_ptr< ListNode<T> > Tail{ nullptr };
+	if (!pRoot)
+		return;
 
-	void InsertToListAsHead(shared_ptr< ListNode<T> > NewListNode)
+	if (pRoot->i < i)
 	{
-		if (Head)
-			NewListNode->Next = Head;
+		DeleteNodeFromTree(pRoot->pRight, i);
+		return;
+	}
+
+	if (pRoot->i > i)
+	{
+		DeleteNodeFromTree(pRoot->pLeft, i);
+		return;
+	}
+
+	Node* q = pRoot;
+	if (!q->pRight)
+		pRoot = q->pLeft;
+	else if (!q->pLeft)
+		pRoot = q->pRight;
+	else
+		DeleteNodeWithTwoChildren(q, q->pLeft);
+
+	delete q;
+}
+
+void PrintTree(Node* pRoot, int Level)
+{
+	if (!pRoot)
+		return;
+
+	PrintTree(pRoot->pRight, Level + 1);
+
+	for (int i = 0; i < Level; i++)
+		cout << "  ";
+	cout << pRoot->i << endl;
+
+	PrintTree(pRoot->pLeft, Level + 1);
+}
+
+void Insert(Node*& pRoot, Node* pNewNode)
+{
+	if (!pRoot)
+		pRoot = pNewNode;
+	else
+	{
+		if (pNewNode->i < pRoot->i)
+			Insert(pRoot->pLeft, pNewNode);
 		else
-			Tail = NewListNode;
-
-		Head = NewListNode;
+			Insert(pRoot->pRight, pNewNode);
 	}
-
-	void InsertToListAsTail(shared_ptr< ListNode<T> > NewListNode)
-	{
-		if (Tail)
-			Tail->Next = NewListNode;
-		else
-			Head = NewListNode;
-
-		Tail = NewListNode;
-	}
-
-	void PrintList(shared_ptr< ListNode<T> > LocalHead)
-	{
-		if (!LocalHead)
-			return;
-
-		if (LocalHead != Head)
-			cout << " -> ";
-		cout << LocalHead->data;
-
-		PrintList(LocalHead->Next);
-	}
-
-	void DeleteNodeWithTwoChildren(shared_ptr< ListNode<T> >& q, shared_ptr< ListNode<T> >& p)
-	{
-		if (p->Right)
-		{
-			DeleteNodeWithTwoChildren(q, p->Right);
-			return;
-		}
-
-		p->data = q->data;
-		q = p;
-		p = p->Left;
-	}
-
-	void DeleteNodeFromList(shared_ptr< ListNode<T> > n, T d)
-	{
-		shared_ptr< ListNode<T> > ProspectNode = (n == nullptr ? Head : n->Next);
-
-		if (ProspectNode->data == d)
-		{
-			if (n == nullptr)
-				Head = Head->Next;
-			else
-				n->Next = (n->Next ? n->Next->Next : nullptr);
-			return;
-		}
-
-		DeleteNodeFromList(ProspectNode, d);
-	}
-
-	T FindInList(shared_ptr< ListNode<T> > LocalHead, T d)
-	{
-		if (!LocalHead)
-			return T::null();
-
-		if (LocalHead->data == d)
-			return LocalHead->data;
-
-		return FindInList(LocalHead->Next, d);
-	}
-
-public:
-
-	void InsertToHead(T d)
-	{
-		InsertToListAsHead(make_shared< ListNode<T> >(d));
-	}
-	void InsertToTail(T d)
-	{
-		InsertToListAsTail(make_shared< ListNode<T> >(d));
-	}
-	void Delete(T d)
-	{
-		DeleteNodeFromList(nullptr, d);
-	}
-	T Find(T d)
-	{
-		return FindInList(Head, d);
-	}
-	void Print()
-	{
-		PrintList(Head);
-	}
-};
+}
 
 void main()
 {
-	vector<pair<int, string>> v{ {3, "a"}, {5, "b"}, {2, "c"} };
+	int i;
 
-	List<MyData> t;
-	for (auto [i, name] : v)
+	Node* pRoot = nullptr;
+	while (true)
 	{
-		t.InsertToHead({ i, name });
+		cin >> i;
+		if (i == 99)
+			break;
+
+		Node* p = new Node(i);
+		InsertToTree(pRoot, p);
 	}
-	t.Print();
 
-	cout << endl << endl;
-
-	List<MyData> t2;
-	for (auto [i, name] : v)
-	{
-		t2.InsertToTail({ i, name });
-	}
-	t2.Print();
-
-	cout << endl << endl;
-
-	t2.Delete(2);
-	t2.Print();
-
-	cout << endl << endl;
-
-	auto x = t2.Find(3);
-	cout << x;
-
-	cout << endl << endl;
-
-	auto y = t2.Find(30);
-	cout << y;
+	PrintTree(pRoot, 1);
 }
